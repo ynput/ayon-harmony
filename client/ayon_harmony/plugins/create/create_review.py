5631435fd089676f16366da08b324b5b529ac940
Petr Kalis <petr.kalis@gmail.com>
2025-08-19 17:10:42 +0200

    Added new auto creator for review

---
+++ client/ayon_harmony/plugins/create/create_review.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Creator plugin for creating workfiles."""
+from ayon_harmony.api.plugin import HarmonyAutoCreator
+
+
+class CreateReview(HarmonyAutoCreator):
+    """Review auto-creator."""
+    identifier = "io.ayon.creators.harmony.review"
+    label = "Review"
+    product_type = "review"
+    icon = "ei.video"
+
+    default_variant = "Main"
+
+    # Placeholder node name for where we store the workfile data.
+    # This does not create an actual Harmony node, but just uses this name
+    # as key in the AYON Harmony scene data.
+    _node_name = "__review__"
