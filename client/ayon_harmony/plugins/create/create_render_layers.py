817ba3f82b2c84e2e7fd0ccc50c7c541c52a92f3
Petr Kalis <petr.kalis@gmail.com>
2026-02-23 17:21:30 +0100

    Bail early
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_harmony/plugins/create/create_render_layers.py
+++ client/ayon_harmony/plugins/create/create_render_layers.py
@@ -299,11 +299,12 @@ class CreateRenderLayer(HarmonyRenderCreator):
         layers_data = get_layers_info()
         layers_full_names = []
         for layer in layers_data:
-            if layer["color"] == group_id:
-                if layer["isGrouped"]:
-                    layers_full_names = [layer["parentPath"]]
-                    break
-                layers_full_names.append(layer["fullName"])
+            if layer["color"] != group_id:
+                continue
+            if layer["isGrouped"]:
+                layers_full_names = [layer["parentPath"]]
+                break
+            layers_full_names.append(layer["fullName"])
 
         self_name = self.__class__.__name__
         created_node = harmony.send(
