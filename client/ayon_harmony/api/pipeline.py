6568092ea3a9d197359932abd7f1088c734800f4
Decnox <loick.ca@hotmail.fr>
2026-03-26 12:43:09 +0100

    update code

--- client/ayon_harmony/api/pipeline.py
+++ client/ayon_harmony/api/pipeline.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 import logging
-
+import ast
 import pyblish.api
 
 from ayon_core.lib import register_event_callback
@@ -43,8 +43,6 @@ LOAD_PATH = os.path.join(PLUGINS_DIR, "load")
 CREATE_PATH = os.path.join(PLUGINS_DIR, "create")
 INVENTORY_PATH = os.path.join(PLUGINS_DIR, "inventory")
 
-script_dir = Path(__file__).parent
-LOG = f"{script_dir}" + "/LOG.txt"
 
 class HarmonyHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
     name = "harmony"
@@ -258,7 +256,7 @@ def ls():
         harmony.get_all_top_names() | harmony.get_palettes_paths()
     )
 
-    cleaned_scene_data = False
+    updated_scene_data = False
     for entity_name, entity_data in scene_data.copy().items():
         if not is_container_data(entity_data):
             continue
@@ -266,46 +264,39 @@ def ls():
         # Filter orphaned containers
         if entity_name not in containers_names:
             del scene_data[entity_name]
-            cleaned_scene_data = True
+            updated_scene_data = True
             continue
 
         if not entity_data.get("objectName"):  # backward compatibility
             entity_data["objectName"] = entity_data["name"]
         yield entity_data
 
-    # nodes = harmony.send(
-    #     {"function": "node.getNodes", "args": ["NOTE"]}
-    # )["result"]
-
-    # for node in nodes:
-    #     if node.split("-")[0] == "Top/templateID":
-    #         data = harmony.send(
-    #             {"function": "node.getTextAttr", "args": [node, 1.0, "text"]}
-    #         )["result"]
-    #         break
-    
+    # to look for new containers pasted to the scene
     func = """function returnNodeAttr() {
     var nodes = node.getNodes(["NOTE"]);
+    var results = [];
     for (var i = 0; i < nodes.length; i++) {
         if (nodes[i].split("-")[0] === "Top/templateID") {
-            return node.getTextAttr(nodes[i], 1.0, "text");
+            var data = node.getTextAttr(nodes[i], 1.0, "text");
+            results.push(data);
+            }
         }
-    }
-    return null;
+        return results;
     }
     returnNodeAttr"""
 
-    data = harmony.send({"function": func})["result"]
+    note_node_text = harmony.send({"function": func})["result"]
 
-    with open(LOG, "a") as f:
-        f.write(f"#################\n")
-        f.write(f"get_all_top_names : {harmony.get_all_top_names()}\n")
-        f.write(f"containers_names : {containers_names}\n")
-        f.write(f"scene_data : {scene_data}\n")
-        f.write(f"data : {data}\n")
-        
-    # Update scene data if cleaned
-    if cleaned_scene_data:
+    note_dict = {}
+    for note in note_node_text:
+        note_dict |= ast.literal_eval(note)
+
+    for entity_name, entity_data in note_dict.items():
+        if entity_name not in scene_data:
+            updated_scene_data = True
+            scene_data[entity_name] = entity_data
+
+    if updated_scene_data:
         harmony.set_scene_data(scene_data)
 
 
