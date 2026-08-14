d401160954319552cf3fdde73ced1435243fa0eb
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-08-14 13:51:20 +0200

    remove product type from base class

--- client/ayon_harmony/api/plugin.py
+++ client/ayon_harmony/api/plugin.py
@@ -207,7 +207,6 @@ class HarmonyRenderCreator(HarmonyCreator):
 
     It creates new Composite type node from which it is rendered.
     """
-    product_type = "render"
 
     node_type = "COMPOSITE"
     # should node be auto connected to main Composite node for Harmony Advanced
